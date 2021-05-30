bd4dabf80563e6443e2756372f13bb866908f01d
DiegoTGJ <DiegoTGjobs@gmail.com>
2021-05-30 16:03:24 -0700

    Updated admin to support custom user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mqe0rmdnbft&3moptb537d1=)uceyl!zb1uj6lpia7o_$01=0t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.18.0.2']
 
 
 # Application definition
